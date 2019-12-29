ad37e6bfb2443a60e60127de404e596d1d32c8a5
AshishDatagrokr <56715295+AshishDatagrokr@users.noreply.github.com>
2019-12-29 23:26:21 +0530

    Update ques5.py

--- ques5.py
+++ ques5.py
@@ -1,26 +1,22 @@
-def search(arr ,start, last, key):
-    if (last >= start ):
+""" Implementing Binary Search """
+# pylint: disable=no-else-return
+def search(array, start, last, key):
+    """ defining search function """
+    if last >= start:
      #   print("hello")
-        
-        mid =int((start +last)/2)
+
+        mid = int((start +last)/2)
        # print(mid)
 
-        if(arr[mid]== key):
+        if array[mid] == key:
             return mid
-        elif (arr[mid] > key ):
-            return search( arr,start,mid-1,key)
+        elif array[mid] > key:
+            return search(array, start, mid-1, key)
         else:
-            return search( arr, mid+1, last,key)
+            return search(array, mid+1, last, key)
     else:
         #print("world")
         return -1
 
-
-
-arr = [1,2,3,5,9,10,12]
-
-print(search(arr,0,6,99))
-
-
-
-        
+ARRAY = [1, 2, 3, 5, 9, 10, 12]
+print(search(ARRAY, 0, 6, 99))
