ef799a084b088ba8f3a42effbc41ab6c014ba2d5
AshishDatagrokr <56715295+AshishDatagrokr@users.noreply.github.com>
2019-12-29 23:21:28 +0530

    Update ques1.py

--- ques1.py
+++ ques1.py
@@ -1,5 +1,8 @@
+"""
+importing mysql connector to establish connection with sql server
+"""
+# pylint: disable=broad-except,import-error
 import mysql.connector
-
 
 def execute_query_to_point_table():
     """"Execute sql query to  point employee_details table"""
@@ -19,23 +22,19 @@ def get_ten_records_of_employee_details_of_table():
         else:
             break
 
-
 def print_each_record_list_wise(many_records):
     """" printing one by one records from 10 records"""
-
     for record in many_records:
         print(list(record))
-
-
 if __name__ == "__main__":
     try:
-        MY_DB = mysql.connector.connect(host="localhost", user="root", password="123@abc#", database="employee_schema") 
+        MY_DB = mysql.connector.connect(host="localhost", user="root", password="123@abc#", database="employee_schema")
         MY_CURSOR = MY_DB.cursor()
         execute_query_to_point_table()
         for records in get_ten_records_of_employee_details_of_table():
             print_each_record_list_wise(records)
 
-    except Exception as exception_name:   #pylint : disable = W0703
+    except Exception as exception_name:
         print(exception_name)
     finally:
         MY_CURSOR.close()
