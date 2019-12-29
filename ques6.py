b8e00367d0e2a2d8cc9fd8eab0c41e49f726a201
AshishDatagrokr <56715295+AshishDatagrokr@users.noreply.github.com>
2019-12-29 23:27:22 +0530

    Update ques6.py

--- ques6.py
+++ ques6.py
@@ -1,13 +1,14 @@
+""" Implementing generator """
 def gen_number():
+    """ checking number which is divided by both 5 and 7 """
     for num in range(INPUT_NUMBER):
         if num % 5 == 0 and num % 7 == 0:
             yield num
 
-s= ""
+RESULT = ""
 INPUT_NUMBER = int(input(" Enter number "))
 for gen_num in gen_number():
    # print(gen_num, end=",")
-    s=s+str(gen_num)+","
+    RESULT = RESULT+str(gen_num)+","
 
-print(s[ :-1])
-
+print(RESULT[ :-1])
