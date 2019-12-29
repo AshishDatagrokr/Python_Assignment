8bccc4bbcc7c87cfc425ce71459b6867a1a51bb4
AshishDatagrokr <56715295+AshishDatagrokr@users.noreply.github.com>
2019-12-29 23:24:29 +0530

    Update ques4.py

--- ques4.py
+++ ques4.py
@@ -1,10 +1,7 @@
-row = 3
-col = 5;
-dep = 8;
-array = [[ [0 for i in range(row)] for j in range(col)] for k in range(dep)]
+""" defining three dimenssion of the list """
+ROW = 3
+COL = 5
+DEPTH = 8
+ARRAY = [[[0 for i in range(ROW)] for j in range(COL)] for k in range(DEPTH)]
 
-print (array)
-
-
-arr = [1,2,5,1,6,8]
-print (arr[1])
+print(ARRAY)
