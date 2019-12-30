254d37a5d40d8237f6255dff0d3a120d8ec2b3ef
AshishDatagrokr <56715295+AshishDatagrokr@users.noreply.github.com>
2019-12-30 10:41:56 +0530

    Update ques10.py

--- ques10.py
+++ ques10.py
@@ -1,6 +1,10 @@
+""" Rversing String"""
+
 class Reverselter:
-    def __init__(self,value):
-        for number in range(len(value)-1,-1,-1):
+    """ defining class"""
+    def __init__(self, value):
+        """ Taking list in value argument"""
+        for number in range(len(value)-1, -1, -1):
             print(value[number])
-value=[11,12,13,14,15]
-Reverselter(value)
+VALUE = [11, 12, 13, 14, 15]
+Reverselter(VALUE)
